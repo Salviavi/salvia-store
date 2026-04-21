c9b6db23029a1fbccc0b2d93ea4cfa581cbce675
Salvia <entp7w8@gmail.com>
2026-04-21 20:30:16 +0700

    added prisma.ts inside db to convert prisma decimal to string, added product detail page with slug

---
+++ app/(root)/product/[slug]/page.tsx
@@ -0,0 +1,87 @@
+import { getProductBySlug } from "@/lib/actions/product.actions";
+import { notFound } from "next/navigation";
+
+import ProductPrice from "@/components/shared/product/product-price";
+
+// shadcn components
+import { Badge } from "@/components/ui/badge";
+import { Button } from "@/components/ui/button";
+import { Card, CardContent } from "@/components/ui/card";
+
+const ProductDetailsPage = async (props: {
+  params: Promise<{ slug: string }>;
+}) => {
+  const { slug } = await props.params;
+
+  const product = await getProductBySlug(slug);
+
+  if (!product) notFound();
+
+  return (
+    <>
+      <section>
+        <div className="grid grid-cols-1 md:grid-cols-5">
+          {/* Images Column */}
+          <div className="col-span-2">{/* Images Component */}</div>
+
+          {/* Details Column */}
+          <div className="col-span-2 p-4">
+            <div className="flex flex-col gap-6">
+              <p>
+                {product.brand} {product.category}
+              </p>
+              <h1 className="h3-bold">{product.name}</h1>
+              <p>
+                {product.rating} of {product.numReviews} Reviews
+              </p>
+              <div className="flex flex-col sm:flex-row sm:items-center gap-4">
+                <ProductPrice
+                  value={Number(product.price)}
+                  className="w-20 rounded-full bg-green-100 text-green-800 px-4 py-2"
+                />
+              </div>
+            </div>
+            <div className="mt-12">
+              <p className="font-semibold">Description</p>
+              <p className="mt-4">{product.description}</p>
+            </div>
+          </div>
+
+          {/* Action Column */}
+          <div>
+            <Card>
+              <CardContent>
+                <div className="mb-2 flex justify-between">
+                  <div>Price</div>
+                  <div>
+                    <ProductPrice value={Number(product.price)} />
+                  </div>
+                </div>
+                <div className="mb-4 flex justify-between">
+                  <div>Status</div>
+                  {product.stock > 0 ? (
+                    <Badge
+                      variant="outline"
+                      className="bg-green-100 border-none"
+                    >
+                      In Stock
+                    </Badge>
+                  ) : (
+                    <Badge variant="destructive">Out of Stock</Badge>
+                  )}
+                </div>
+                {product.stock > 0 && (
+                  <div>
+                    <Button className="w-full">Add to Cart</Button>
+                  </div>
+                )}
+              </CardContent>
+            </Card>
+          </div>
+        </div>
+      </section>
+    </>
+  );
+};
+
+export default ProductDetailsPage;
---
+++ db/prisma.ts
@@ -0,0 +1,20 @@
+import { PrismaClient } from "@prisma/client";
+
+// Extends the PrismaClient with a custom result transformer to convert the price and rating fields to strings.
+
+export const prisma = new PrismaClient().$extends({
+  result: {
+    product: {
+      price: {
+        compute(product) {
+          return product.price.toString();
+        },
+      },
+      rating: {
+        compute(product) {
+          return product.rating.toString();
+        },
+      },
+    },
+  },
+});
